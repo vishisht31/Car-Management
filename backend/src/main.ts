a7c7da9b7c8b5629aa3d45f43aa72de5ac99441d
vishisht31 <vishishtmaroria31@gmail.com>
2025-01-30 01:01:23 +0530

    final commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,16 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { writeFileSync } from 'fs';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{cors:true});
   AppModule.configureSwagger(app);
+  app.useStaticAssets(path.join(__dirname, '..', 'public', 'dist'));
 
+  // Redirect all routes to index.html for React/Vite SPA
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'dist', 'index.html'));
+  });
   const document = SwaggerModule.createDocument(
     app,
     new DocumentBuilder().build(),
   );
   writeFileSync('./swagger.json', JSON.stringify(document, null, 2));
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
